df2136a0b6846fc94573e7c27349ba8c5a12e7ce
Rahul Kumar <93612455+Rahulrockss@users.noreply.github.com>
2024-05-23 20:17:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "6912029702:AAHr81j_46DqvqsUKikiJUqRqXpSQ6DMKqo"  # Your telegram bot token
+DEVELOPER_ID = 1239172280  # Your telegram id (used for error reporting and private mode)
+IS_BOT_PRIVATE = False  # Change this to True to make bot private
